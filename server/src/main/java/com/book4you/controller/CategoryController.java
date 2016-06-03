c3b9e630a71c5806ae002ee45ee86d52809bee08
Nir Galon <nirgn975@gmail.com>
2016-06-03 18:22:06 +0300

    Add docs.

--- server/src/main/java/com/book4you/controller/CategoryController.java
+++ server/src/main/java/com/book4you/controller/CategoryController.java
@@ -16,9 +16,18 @@ import com.book4you.repository.CategoryRepository;
     @Inject
     private CategoryRepository categoryRepository;
 
+    /**
+     * Get all the categories.
+     *
+     * @return
+     *  All the categories.
+     */
     @RequestMapping(value="/categories", method= RequestMethod.GET)
     public ResponseEntity<Iterable<Category>> getAllCategories() {
+        // Get all the categories in the categories repository.
         Iterable<Category> allCategories = categoryRepository.findAll();
+
+        // Return all the categories in the response with "OK" status code.
         return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 }
