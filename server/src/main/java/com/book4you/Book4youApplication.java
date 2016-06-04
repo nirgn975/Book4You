0f719cc088b637dd487c8b7a90df70a40db7b4ab
Nir Galon <nirgn975@gmail.com>
2016-06-04 14:08:31 +0300

    books works.

---
+++ server/src/main/java/com/book4you/Book4youApplication.java
@@ -0,0 +1,12 @@
+package com.book4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book4youApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Book4youApplication.class, args);
+	}
+}
