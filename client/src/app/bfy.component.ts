9e7cd98b0dc20bdeba52ec29ef4301b98956e9a9
Nir Galon <nirgn975@gmail.com>
2016-06-13 09:32:54 +0300

    Refactoring to ng-cli.

---
+++ client/src/app/bfy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'bfy-app',
+  templateUrl: 'bfy.component.html',
+  styleUrls: ['bfy.component.css']
+})
+export class BfyAppComponent {
+  title = 'bfy works!';
+}
