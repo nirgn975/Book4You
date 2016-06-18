5de928f2630cf12ad60efca3fb66fe42e9d77945
Nir Galon <nirgn975@gmail.com>
2016-06-18 17:00:48 +0300

    Edit book model.

--- client/src/app/+books/shared/book.model.spec.ts
+++ client/src/app/+books/shared/book.model.spec.ts
@@ -5,7 +5,7 @@ import {
   iit,
   it
 } from '@angular/core/testing';
-import {Book} from './book';
+import {Book} from './book.model';
 
 describe('Book', () => {
   it('should create an instance', () => {
