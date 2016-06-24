34e602749f479ec342c8dc6f8fdbd368bee5a1b9
Nir Galon <nirgn975@gmail.com>
2016-06-24 15:47:10 +0300

    Add nav (#25)
    * Add nav component.
    
    * Finish styling the header.
    
    * Style the search input, and add cart dummy data.
    
    * Cleanup.

---
+++ client/src/app/shared/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-nav',
+  templateUrl: 'nav.component.html',
+  styleUrls: ['nav.component.css']
+})
+
+export class NavComponent {
+
+  constructor() {}
+
+}
