b33790a2e37088069bbcfceab0940a596dc4c7f1
Nir Galon <nirgn975@gmail.com>
2016-07-03 20:42:38 +0300

    API version. (#34)
    * Add gradle to gitignore.
    
    * Add api version, remove paging, and fix data improt.
    
    * Remove gradle directory.

--- server/src/main/java/com/book4you/book/BookRepository.java
+++ server/src/main/java/com/book4you/book/BookRepository.java
@@ -1,13 +1,6 @@
 package com.book4you.book;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
-
-    @RestResource(rel = "title-contains", path = "containsTitle")
-    Page<Book> findByTitleContaining(@Param("title") String title, Pageable page);
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
--- server/src/main/java/com/book4you/core/BaseEntity.java
+++ server/src/main/java/com/book4you/core/BaseEntity.java
@@ -1,15 +1,15 @@
 package com.book4you.core;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private final Long id;
+
+    @Version
+    private Long version;
 
     protected BaseEntity() {
         id = null;
