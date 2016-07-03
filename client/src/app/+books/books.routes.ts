a8d4e0ca54193420182e7002f848a5771714df4b
Nir Galon <nir@galons.io>
2016-07-03 21:00:38 +0300

    Update angular version. (#33)
    * Update angular version.
    
    * Fix router.
    
    * Add spring build to gitignore.

--- client/src/app/+books/books.routes.ts
+++ client/src/app/+books/books.routes.ts
@@ -6,19 +6,11 @@ import { BookDetailsComponent }  from './book-details/book-details.component';
 
 export const BooksRoutes: RouterConfig = [
   {
-    path: '',
-    redirectTo: '/category/1',
-    terminal: true
-  },
-  { path: 'category/:categoryId', component: BooksListComponent },
-  { path: 'category/:categoryId/book/:bookId', component: BookDetailsComponent },
-
-  // {
-  //   path: 'category',
-  //   component: BooksComponent,
-  //   children: [
-  //     { path: '/:categoryId', component: BooksListComponent },
-  //     { path: '/:categoryId/book/:bookId', component: BookDetailsComponent },
-  //   ]
-  // }
+    path: 'category',
+    component: BooksComponent,
+    children: [
+      { path: ':categoryId', component: BooksListComponent },
+      { path: ':categoryId/book/:bookId', component: BookDetailsComponent },
+    ]
+  }
 ];
