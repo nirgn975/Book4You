677fd2d86dd7bcd87cac53985d5ea78ab1278494
Nir Galon <nirgn975@gmail.com>
2016-07-29 20:58:23 +0300

    Try to post a new category.

--- client/src/app/main-content/shared/main-content.service.ts
+++ client/src/app/main-content/shared/main-content.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions  } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MainContentService {
 
-  constructor(private http: Http) {}
+  constructor(
+    private http: Http) {}
 
-  addNewCategory(categoryName: String) {
-    console.log(categoryName);
+  addNewCategory(categoryName: string) {
+    let body = JSON.stringify({ name: categoryName });
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    console.log(body);
+    console.log(options);
+
+    return this.http.post('http://localhost:8080/api/v1/categories', body, options)
+                .map(this.extractData)
+                .catch(this.handleError);
   }
 
-  private handleError(error: Response) {
+  private extractData(res: Response) {
+    let body = res.json();
+    console.log(body);
+    return body.data || { };
+  }
+
+  private handleError (error: any) {
     console.error(error);
     return Observable.throw(error.json().error || 'Server error');
   }
-
 }
