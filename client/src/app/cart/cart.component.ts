b704d54e359dedb5179ef782782235506ca624aa
Nir Galon <nirgn975@gmail.com>
2016-08-13 19:15:51 +0300

    Show books in cart.

--- client/src/app/cart/cart.component.ts
+++ client/src/app/cart/cart.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { CartService } from './shared/cart.service';
+import { Book } from '../content/shared/book.model';
 
 @Component({
   moduleId: module.id,
   selector: 'bfy-cart',
   templateUrl: 'cart.component.html',
-  styleUrls: ['cart.component.css']
+  styleUrls: ['cart.component.css'],
+  providers: [CartService]
 })
 export class CartComponent implements OnInit {
+  cart: Observable<Book[]>;
+  totalValue: number;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+    this.cart = this.cartService.getCartUser(46);
   }
-
 }
