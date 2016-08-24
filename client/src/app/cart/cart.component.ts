a3b3fc3a889d2632fbd3cdc0dfc2488070c369bb
Nir Galon <nirgn975@gmail.com>
2016-08-24 20:51:52 +0300

    Add order form.

--- client/src/app/cart/cart.component.ts
+++ client/src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { FormGroup, Validators, FormBuilder,
+  REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
 
 import { AuthenticationService } from '../shared/authentication.service';
 import { CartService } from './shared/cart.service';
@@ -11,10 +13,12 @@ import { Cart } from './shared/cart.model';
   selector: 'bfy-cart',
   templateUrl: 'cart.component.html',
   styleUrls: ['cart.component.css'],
+  directives: [REACTIVE_FORM_DIRECTIVES],
   providers: [CartService, AuthenticationService]
 })
 export class CartComponent implements OnInit {
   private cart: Cart = new Cart();
+  private orderForm: FormGroup;
   books: Observable<Book[]>;
   totalValue: number;
   auth: string;
@@ -23,8 +27,16 @@ export class CartComponent implements OnInit {
 
   constructor(
     private authenticationService: AuthenticationService,
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private fb: FormBuilder
+  ) {
+    this.orderForm = fb.group({
+        "fullName": ["", Validators.required],
+        "street": ["", Validators.required],
+        "houseNumber": ["", Validators.required],
+        "city": ["", Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.auth = this.authenticationService.getAuth();
@@ -46,6 +58,22 @@ export class CartComponent implements OnInit {
     console.log(this.cart);
     this.cartService.removeBookFromCart(this.options, this.cart.id, bookId).subscribe(
       (data) => location.reload()
+    );
+  }
+
+  onSubmitOrder(event) {
+    event.preventDefault();
+    let auth = this.authenticationService.getAuth();
+    let options = this.authenticationService.getOptions(auth);
+    let order = this.orderForm['_value'];
+
+
+    order['totalAmount'] = this.cart.totalAmount;
+    this.books.subscribe(
+      (data) => {
+        order['books'] = data;
+        console.log(order);
+      }
     );
   }
 }
