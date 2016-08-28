8adedee1dc245620039f34423e3ba3a039534511
Nir Galon <nirgn975@gmail.com>
2016-08-29 00:17:17 +0300

    Add simple Book tests.

--- server/src/test/java/com/book4you/book/BookTest.java
+++ server/src/test/java/com/book4you/book/BookTest.java
@@ -10,11 +10,35 @@ import static org.junit.Assert.*;
 
 public class BookTest {
     private Utils utils = new Utils();
+    Book book;
+
+    @Before
+    public void setUp() throws Exception {
+        book = new Book("Title", "Author", "Description", 10, utils.Image2ByteArray("images/FallingAngel.jpg"), 1);
+    }
 
     @Test
     public void getTitle() throws Exception {
-        Book book = new Book("Title", "Author", "Description", 10, utils.Image2ByteArray("images/FallingAngel.jpg"), 0);
-
         assertEquals("Title", book.getTitle());
+    }
+
+    @Test
+    public void getAuthor() throws Exception {
+        assertEquals("Author", book.getAuthor());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+        assertEquals("Description", book.getDescription());
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+        assertEquals(10, book.getPrice());
+    }
+
+    @Test
+    public void getInventory() throws Exception {
+        assertEquals(1, book.getInventory());
     }
 }
