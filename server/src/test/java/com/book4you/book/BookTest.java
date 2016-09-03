d8c038fd49108067364697fba5607a3d68d32e9d
Nir Galon <nir@galons.io>
2016-09-03 18:45:28 +0300

    Add more unit tests. (#75)
    * Add simple Book tests.
    
    * Add before import.
    
    * Fix category-header.
    
    * Add category and user tests.

--- server/src/test/java/com/book4you/book/BookTest.java
+++ server/src/test/java/com/book4you/book/BookTest.java
@@ -1,5 +1,6 @@
 package com.book4you.book;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.book4you.utils.Utils;
@@ -7,14 +8,68 @@ import com.book4you.book.Book;
 
 import static org.junit.Assert.*;
 
-
 public class BookTest {
     private Utils utils = new Utils();
+    Book book;
+
+    @Before
+    public void setUp() throws Exception {
+        book = new Book("Title", "Author", "Description", 10, utils.Image2ByteArray("images/FallingAngel.jpg"), 1);
+    }
 
     @Test
     public void getTitle() throws Exception {
-        Book book = new Book("Title", "Author", "Description", 10, utils.Image2ByteArray("images/FallingAngel.jpg"), 0);
-
         assertEquals("Title", book.getTitle());
+    }
+
+    @Test
+    public void setTitle() throws Exception {
+        book.setTitle("newTitle");
+        assertEquals("newTitle", book.getTitle());
+    }
+
+    @Test
+    public void getAuthor() throws Exception {
+        assertEquals("Author", book.getAuthor());
+    }
+
+    @Test
+    public void setAuthor() throws Exception {
+        book.setAuthor("newAuthor");
+        assertEquals("newAuthor", book.getAuthor());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+        assertEquals("Description", book.getDescription());
+    }
+
+    @Test
+    public void setDescription() throws Exception {
+        book.setDescription("newDescription");
+        assertEquals("newDescription", book.getDescription());
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+        assertEquals(10, book.getPrice());
+    }
+
+    @Test
+    public void setPrice() throws Exception {
+        book.setPrice(20);
+        assertEquals(20, book.getPrice());
+    }
+
+    @Test
+    public void getInventory() throws Exception {
+        assertEquals(1, book.getInventory());
+    }
+
+    @Test
+    public void setInventory() throws Exception {
+        assertEquals(1, book.getInventory());
+        book.setInventory(3);
+        assertEquals(3, book.getInventory());
     }
 }
